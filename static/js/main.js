8dbfe451447c184660cf04d0daed223d0c5839b8
MikeT <mikejamesthompson@gmail.com>
2015-05-16 14:26:53 +0100

    Part way through setting up dynamic schedule stuff

--- static/js/main.js
+++ static/js/main.js
@@ -2,11 +2,20 @@ $(document).ready(function(){
 
 	var reminder_form = $('#reminder-setup');
 
-	var start_hour = "0700"
+	var audio_messages = [];
+
+	var schedule = {
+
+		1: ["8:00am"],
+		2: ["8:00am", "5:00pm"],
+		3: ["8:00am", "12:00pm", "5:00pm"],
+		4: ["8:00am", "11:00am", "2:00pm", "5:00pm"]
+
+	}
 
 	
-	// Capture form submit and send ajax request
-	// with data
+	// Capture form submit and send ajax 
+	// request with data
 
 	reminder_form.submit(function(e) {
 
@@ -19,13 +28,28 @@ $(document).ready(function(){
 	});
 
 	// Configure time to take based on frequency
+
 	var frequency_selector = reminder_form.find('#frequency');
+
+	var reminder_schedule_list = reminder_form.find('#reminder_schedule');
 	
 	frequency_selector.change(function() {
 
-		console.log(frequency_selector.val());
+		var times = schedule[frequency_selector.val()];
 
-	})
+		var template = $('#reminder_template').clone();
+
+		reminder_schedule_list.empty()
+
+		times.forEach(function(e){ 
+
+			var time = template.clone().appendTo(reminder_schedule_list);
+
+			time.val();
+
+		});
+
+	});
 
 
 });
